1a29902217ba79696bf6b25f110b0dbfb9379477
ansariali <=>
2025-06-17 11:38:58 +0530

    done with header and footer

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer>
-      <p>This is App footer</p>
+    <footer className="h-[10vh] shadow-lg flex items-center justify-center text-gray-600">
+      <p>Code and Learn by Ansari Ali</p>
     </footer>
   );
 };
--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export const Header = () => {
   return (
-    <header>
-      <h2>My site Header components</h2>
+    <header className="h-[10vh] flex justify-between items-center px-8 shadow-lg">
+      <img src="/vite.svg" alt="vite" />
+      <nav className="flex gap-7">
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/contact"}>Contact</NavLink>
+        <NavLink to={"/sign-in"}>Sign in</NavLink>{" "}
+      </nav>
     </header>
   );
 };
